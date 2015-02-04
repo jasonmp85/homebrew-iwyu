80fcc5685c4fea2b6efa5844919eee59a188e7ba
Jason Petersen <jason@citusdata.com>
2015-02-04 14:46:11 -0800

    Correct style violations
    Hooray for rubocop!

--- iwyu.rb
+++ iwyu.rb
@@ -1,15 +1,15 @@
-require 'English'
-require 'formula'
+require "English"
+require "formula"
 
 # include-what-you-use needs access to headers included with LLVM 3.5, which
 # is only present in Xcode version 6.0 or higher.
 class Xcode6 < Requirement
   fatal true
 
-  satisfy { MacOS::Xcode.version >= '6.0' }
+  satisfy { MacOS::Xcode.version >= "6.0" }
 
   def message
-    'Xcode 6.0 or newer is required for this package.'
+    "Xcode 6.0 or newer is required for this package."
   end
 end
 
@@ -18,32 +18,32 @@ end
 # build and install include-what-you-use, symlink it as iwyu, and install a
 # Python wrapper to automatically correct includes (fix_include).
 class Iwyu < Formula
-  CLANG_VERSION = '3.5'
+  CLANG_VERSION = "3.5"
 
-  homepage 'https://code.google.com/p/include-what-you-use/'
-  url 'http://include-what-you-use.com/downloads/' +
+  homepage "https://code.google.com/p/include-what-you-use/"
+  url "http://include-what-you-use.com/downloads/" \
     "include-what-you-use-#{Iwyu::CLANG_VERSION}-macosx-apple-darwin.tar.gz"
-  sha1 '7f38d22c3da1c9924179b522a9f4299373cddcd8'
-  version '0.3'
+  sha1 "7f38d22c3da1c9924179b522a9f4299373cddcd8"
+  version "0.3"
 
   depends_on Xcode6
 
   def install
     clang_libs = "#{MacOS::Xcode.toolchain_path}/usr/lib/clang/6.0"
-    iwyu_clang_path = (lib / 'clang')
+    iwyu_clang_path = (lib / "clang")
 
     iwyu_clang_path.mkpath
     iwyu_clang_path.install_symlink(clang_libs => "#{Iwyu::CLANG_VERSION}.0")
 
-    bin.install('fix_includes.py' => 'fix_include')
-    bin.install('include-what-you-use')
-    bin.install_symlink('include-what-you-use' => 'iwyu')
+    bin.install("fix_includes.py" => "fix_include")
+    bin.install("include-what-you-use")
+    bin.install_symlink("include-what-you-use" => "iwyu")
   end
 
   test do
     # write out a header and a C file relying on transitive dependencies
-    (testpath / 'demo.h').write('#include <stdio.h>')
-    (testpath / 'demo.c').write <<-EOS.undent
+    (testpath / "demo.h").write("#include <stdio.h>")
+    (testpath / "demo.c").write <<-EOS.undent
     #include "demo.h"
 
     int main(void)
@@ -54,7 +54,7 @@ class Iwyu < Formula
     fixes = shell_output "iwyu #{testpath}/demo.c 2>&1", 1
 
     # pass the output to the fixer script and assert that it fixed one file
-    results = pipe_output 'fix_include', fixes
+    results = pipe_output "fix_include", fixes
 
     assert_match(/IWYU edited 1 file/, results)
 
